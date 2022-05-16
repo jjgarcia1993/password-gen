aa925516c16979dba41c3ac9603a73fe646a0a3f
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-05-15 21:49:05 -0400

    not sure?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
 // Assignment code here
 
-writePassword();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var result = document.querySelector("card-header");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(14, false);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+writePassword();
